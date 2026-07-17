d878dbb9bd133271e77107111568177eafab0ac5
recoveryrob83-lab <recoveryrob83@gmail.com>
2026-07-16 22:40:50 -0500

    Export Today dashboard adapters

--- apps/lifeos-dashboard/lifeos_dashboard/adapters/__init__.py
+++ apps/lifeos-dashboard/lifeos_dashboard/adapters/__init__.py
@@ -1,13 +1,17 @@
 """Source adapters for LifeOS dashboard data."""
 
 from .base import DashboardSource
+from .calendar_ical import GoogleCalendarIcalDashboardSource
 from .local_github import LocalGitHubDashboardSource
 from .sample import SampleDashboardSource
+from .todoist import TodoistDashboardSource
 from .trello import TrelloFlowDashboardSource
 
 __all__ = [
     "DashboardSource",
+    "GoogleCalendarIcalDashboardSource",
     "LocalGitHubDashboardSource",
     "SampleDashboardSource",
+    "TodoistDashboardSource",
     "TrelloFlowDashboardSource",
 ]
